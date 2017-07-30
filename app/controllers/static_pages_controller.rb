4c0fb5fb0dfa392b6a2ce41ffff161840adacd62
Gerrit Seger <gerrit-cf@byom.de>
2017-07-30 21:14:10 +0200

    Task 3.11

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   rescue_from ActionView::MissingTemplate, with: :render_404
 
   def index
-    static_page = params[:id]
+    static_page = params[:static_page_id]
     render static_page
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
 
-  get 'static_pages/:id', to: 'static_pages#index'
+  # This has to stay the last line in the file to not interfere with other
+  # routes. This way, we can avoid the "static_pages" part of the route and turn
+  # "/static_pages/about" into "/about"
+  get ':static_page_id', to: 'static_pages#index'
 end
