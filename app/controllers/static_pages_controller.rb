28310f98f98e22437047807cb65b84853dc2f7e4
Gerrit Seger <gerrit-cf@byom.de>
2017-08-10 19:44:08 +0200

    Fix static pages security issue

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,26 @@
 class StaticPagesController < ApplicationController
-  rescue_from ActionView::MissingTemplate, with: :render_404
+  STATIC_PAGES = %w[about contact index]
+
+  class StaticPageError < RuntimeError; end
+
+  rescue_from StaticPageError, with: :render_404
+
+  before_action :set_and_verify_static_page!
 
   def index
-    static_page = params[:static_page_id]
     render static_page
+  end
+
+  private
+
+  attr_reader :static_page
+
+  def set_and_verify_static_page!
+    @static_page = params[:static_page_id]
+    raise StaticPageError unless static_page_valid?
+  end
+
+  def static_page_valid?
+    static_page.nil? || static_page.in?(STATIC_PAGES)
   end
 end
