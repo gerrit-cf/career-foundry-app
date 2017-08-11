5dd102ff1fb5391d5602379e7a3e2105633d9751
Gerrit Seger <gerrit-cf@byom.de>
2017-08-11 15:38:32 +0200

    Task 4.1: Create views and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def present(model)
+    presenter_klass = "#{model.class}Presenter".constantize
+    presenter_klass.new(model, self).tap do |presenter|
+      yield presenter if block_given?
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
 
+  resources :products
+
   # This has to stay the last line in the file to not interfere with other
   # routes. This way, we can avoid the "static_pages" part of the route and turn
   # "/static_pages/about" into "/about"
