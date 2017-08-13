123d475db59c56cbe887fc921178abe442270399
Gerrit Seger <gerrit-cf@byom.de>
2017-08-13 22:12:34 +0200

    Only display the first error message if multiple ones are present

--- config/initializers/form_builder.rb
+++ config/initializers/form_builder.rb
@@ -2,8 +2,8 @@ module ActionView::Helpers
   class FormBuilder
     def errors(attribute)
       return if object.errors[attribute].empty?
-      full_messages = object.errors.full_messages_for(attribute).join('<br>')
-      "<div class=\"scoped-field-errors\">#{full_messages}</div>".html_safe
+      full_message = object.errors.full_messages_for(attribute).first
+      "<div class=\"scoped-field-errors\">#{full_message}</div>".html_safe
     end
   end
 end
