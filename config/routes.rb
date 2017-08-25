9a0f72138d3243e909d69b8665689df903cc8b97
Gerrit Seger <gerrit-cf@byom.de>
2017-08-25 20:46:07 +0200

    Task 5.1: Mess up routes for testing purposes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'landing_page#index'
+  root 'products#index'
 
   resources :products
   resources :orders, only: %i[index show create destroy]
+
+  get 'home', to: 'landing_page#index', as: :landing_page
 
   # This has to stay the last line in the file to not interfere with other
   # routes. This way, we can avoid the "static_pages" part of the route and turn
