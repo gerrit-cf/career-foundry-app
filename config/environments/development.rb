753a67688fcdde3a71683401b1372e0598684e6a
Gerrit Seger <gerrit-cf@byom.de>
2017-08-31 18:58:52 +0200

    Task 5.3: Install letter opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Open mail in browser
+  config.action_mailer.delivery_method = :letter_opener
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
