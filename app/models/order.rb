5a6fe51fcc538f391f9a917bff29bb34aed2afdc
Gerrit Seger <gerrit-cf@byom.de>
2017-09-03 15:05:22 +0200

    Task 5.4: Create User model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,4 @@
 class Order < ApplicationRecord
   belongs_to :product
   belongs_to :user
-
-  validates :product, :user, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170820125322) do
+ActiveRecord::Schema.define(version: 20170903130027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 20170820125322) do
     t.datetime "updated_at", null: false
     t.string "colour"
     t.decimal "price", default: "0.0", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
