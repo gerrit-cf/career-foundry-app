1340fd4f23654435a1bc4156b47d58103fdfce32
Gerrit Seger <gerrit-cf@byom.de>
2017-09-08 10:18:25 +0200

    Task 5.4: Validate uniqueness of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :orders
+
+  validates :email, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20170903130027) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
