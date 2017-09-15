fe08d95a102496b428015211fda6ecc6b3283a39
Gerrit Seger <gerrit-cf@byom.de>
2017-09-15 16:17:55 +0200

    Task 5.7: Improve Review validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,7 +3,7 @@ class Review < ApplicationRecord
   belongs_to :reviewable, polymorphic: true
 
   validates :user, :reviewable, presence: true
-  validates :rating, numericality: { allow_nil: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+  validates :rating, numericality: { allow_nil: true, only_integer: true }, inclusion: 1..5
 
   scope :rating_asc,  -> { order('rating ASC') }
   scope :rating_desc, -> { order('rating DESC') }
