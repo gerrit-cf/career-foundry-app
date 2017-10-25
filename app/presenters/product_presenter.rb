399db848169eb8b61fb3132b4d5f7a6184965872
Gerrit Seger <gerrit-cf@byom.de>
2017-10-25 21:00:44 +0200

    Fix review headline

--- app/presenters/product_presenter.rb
+++ app/presenters/product_presenter.rb
@@ -10,7 +10,7 @@ class ProductPresenter < ApplicationPresenter
   def image_url
     # External ressources only for now.
     # We do not want an invalid asset to break our app.
-    return '' if super.nil? || !external_url(super)
+    return '' if super.nil? || !external_url?(super)
     super
   end
 
--- app/presenters/review_presenter.rb
+++ app/presenters/review_presenter.rb
@@ -3,13 +3,6 @@ class ReviewPresenter < ApplicationPresenter
     super.present? ? super : '<i>- No comment provided -</i>'.html_safe
   end
 
-  def headline
-    [
-      user,
-      format('<i>%s ago</i>', time_ago).html_safe
-    ].join('')
-  end
-
   def time_ago
     h.time_ago_in_words(created_at)
   end
