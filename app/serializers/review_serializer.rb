10039ac4d300ce5518450681102c7f4b6d5ac2ce
Gerrit Seger <gerrit-cf@byom.de>
2017-11-25 11:07:22 +0100

    Task 6.14: Add reviewable to review serializer

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -3,6 +3,7 @@ class ReviewSerializer < ApplicationSerializer
 
   attribute(:comment)
   attribute(:name) { object.user.to_s }
+  attribute(:reviewable_id) { object.reviewable.id }
   attribute(:rating)
   attribute(:timestamp) do
     "#{time_ago_in_words(object.created_at)} ago"
