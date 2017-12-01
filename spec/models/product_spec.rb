d126fa8676e17c22747f42d9f0897a46854d6e5c
Gerrit Seger <gerrit-cf@byom.de>
2017-12-01 12:28:17 +0100

    Task 6.15: Add model specs

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,15 +1,46 @@
 require 'rails_helper'
 
 describe Product do
-  let(:first_product) { create :product, name: 'Bike' }
-  let(:second_product) { create :product, name: 'Speedster' }
-  let(:third_product) { create :product, name: 'Eurowing Bike' }
+  let(:product) { create :product, name: 'Bike', price: 2499.99 }
+
+  describe 'validations' do
+    context 'name' do
+      subject { build :product, name: '' }
+
+      it { is_expected.to_not be_valid }
+    end
+
+    context 'price' do
+      let(:product_without_price) { build :product, price: nil }
+      let(:product_with_invalid_price) { build :product, price: -1 }
+
+      it 'validates the price' do
+        expect(product_without_price).to_not be_valid
+        expect(product_with_invalid_price).to_not be_valid
+      end
+    end
+  end
 
   describe '#name_like' do
+    let(:second_product) { create :product, name: 'Speedster' }
+    let(:third_product) { create :product, name: 'Eurowing Bike' }
+
     it 'properly finds products by name' do
       expect(described_class.name_like('speed')).to contain_exactly(second_product)
       expect(described_class.name_like('EURO')).to contain_exactly(third_product)
-      expect(described_class.name_like('bike')).to contain_exactly(first_product, third_product)
+      expect(described_class.name_like('bike')).to contain_exactly(product, third_product)
     end
+  end
+
+  describe '#price_in_cents' do
+    subject { product.price_in_cents }
+
+    it { is_expected.to eq(249999) }
+  end
+
+  describe '#to_s' do
+    subject { product.to_s }
+
+    it { is_expected.to eq('Bike') }
   end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,15 +1,25 @@
 require 'rails_helper'
 
 describe Review do
-  subject { create :review }
-
   describe 'validations' do
-    context 'with an invalid rating' do
-      before do
-        subject.rating = 6
-      end
+    context 'rating' do
+      let(:review_without_rating) { build :review, rating: nil }
+      let(:review_with_invalid_rating) { build :review, rating: 6 }
 
-      it { is_expected.to_not be_valid }
+      it 'validates the rating' do
+        expect(review_without_rating).to_not be_valid
+        expect(review_with_invalid_rating).to_not be_valid
+      end
+    end
+  end
+
+  describe '.created_at_desc' do
+    let!(:first_review) { create :review }
+    let!(:second_review) { create :review }
+    let!(:third_review) { create :review }
+
+    it 'orders descending by create date' do
+      expect(Review.created_at_desc).to eq([third_review, second_review, first_review])
     end
   end
 end
